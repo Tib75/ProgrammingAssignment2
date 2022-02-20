57b7bb229455ccb670189ff117b89ecc11c3b02c
Tib75 <69599197+Tib75@users.noreply.github.com>
2022-02-20 15:51:07 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Those two functions are supposed to :
+## makeCacheMatrix - Set / Get and create a solve cache Matrix
+## cacheSolve - Test the cache and call solve / set solve if its empty
 
-## Write a short comment describing this function
-
+## makeCacheMatrix Function is used to get / set solve
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -18,8 +18,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve is a function aimed at checking if the x parameters is filled or not
+## and get the cached value if its filled or call the solve function and cache it if its empty
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   m <- x$getsolve()
